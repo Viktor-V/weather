f3e01731521c70040222969867e808eda248ba75
Viktor Vipolzov <viktor.vipolzov@makeit.lv>
2023-01-21 20:50:56 +0200

    Autogenerate SSL keys

--- docker/php/docker-entrypoint.sh
+++ docker/php/docker-entrypoint.sh
@@ -61,6 +61,8 @@ if [ "$1" = 'php-fpm' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ]; then
 		fi
 	fi
 
+	bin/console lexik:jwt:generate-keypair --skip-if-exists
+
 	setfacl -R -m u:www-data:rwX -m u:"$(whoami)":rwX var
 	setfacl -dR -m u:www-data:rwX -m u:"$(whoami)":rwX var
 fi
