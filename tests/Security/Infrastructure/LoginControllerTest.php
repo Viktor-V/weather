8993d162cc3740d77ee5d60828c10f8dc353f19e
Viktor Vipolzov <viktor.vipolzov@makeit.lv>
2023-01-22 11:55:50 +0200

    Fixed security configuration + added additional login test

--- tests/Security/Infrastructure/LoginControllerTest.php
+++ tests/Security/Infrastructure/LoginControllerTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Tests\Security\Infrastructure;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
 
 class LoginControllerTest extends WebTestCase
 {
@@ -25,5 +26,24 @@ class LoginControllerTest extends WebTestCase
 
         self::assertNotNull($data['token']);
         self::assertNotNull($data['refreshToken']);
+    }
+
+    public function testWrongCredentials(): void
+    {
+        $client = self::createClient();
+
+        $client->request(
+            'POST',
+            '/api/login',
+            [],
+            [],
+            ['CONTENT_TYPE' => 'application/json'],
+            json_encode(['username' => 'wrong', 'password' => 'wrong'])
+        );
+
+        $data = json_decode($client->getResponse()->getContent(), true);
+        
+        self::assertEquals(Response::HTTP_UNAUTHORIZED, $data['code']);
+        self::assertNotNull('Invalid credentials', $data['message']);
     }
 }
